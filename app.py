3627ef200ccbfea29504ddb361cc95faacd90d39
Manuele Pesenti <manuele@inventati.org>
2017-08-28 15:33:44 +0200

    dev

--- app.py
+++ app.py
@@ -2,3 +2,19 @@
 
 import os
 import sys
+
+os.environ['WEB2PY_PATH'] = "/"
+
+sys.path.append(os.path.join(os.environ['WEB2PY_PATH'], 'web2py'))
+sys.path.append(os.path.join(os.environ['WEB2PY_PATH'], 'web2py', 'gluon'))
+
+WEB2PY_LOG = os.path.join(os.environ['OPENSHIFT_LOG_DIR'],'web2py.log')
+
+from gluon.settings import global_settings
+import gluon.main
+
+application = gluon.main.appfactory(
+    wsgiapp = gluon.main.wsgibase,
+    logfilename = WEB2PY_LOG,
+    profilerfilename = None
+)
