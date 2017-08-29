5f13935399bc6e089b4e361908d33d67bfc425c1
Manuele Pesenti <manuele@inventati.org>
2017-08-29 15:47:16 +0200

    dev

--- app.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -e
-cd wsgi/web2py
-python ./web2py.py -a foo -i 0.0.0.0 -p 8080
--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='web2pyhost',
       version='0.1',
       description='OpenShift App',
       author='Your Name',
       author_email='example@example.com',
       url='http://www.python.org/sigs/distutils-sig/',
-    #   install_requires=['newrelic','GitPython'],
+      install_requires=[
+          'gunicorn',
+        #   'newrelic','GitPython'
+      ],
 )
